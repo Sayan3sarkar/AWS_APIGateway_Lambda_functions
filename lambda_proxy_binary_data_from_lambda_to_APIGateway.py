0d220a724d8b9032eb263511c778a03237c014a5
Sayan Sarkar <sayansarkar333@gmail.com>
2020-07-17 11:51:09 +0530

    File added for sending binary data from AWS S3 to API Gateway via AWS Lambda function through lambda proxy integration

---
+++ lambda_proxy_binary_data_from_lambda_to_APIGateway.py
@@ -0,0 +1,27 @@
+# Sends binary data from AWS S3 to API Gateway via AWS Lambda function through lambda proxy integration
+
+import json
+import base64
+import boto3
+
+def lambda_handler(event, context):
+    
+    s3 = boto3.client("s3")    
+    
+    # for lambda proxy integration 
+    bucket_name = event['pathParameters']['bucket']
+    file_name = event['queryStringParameters']['file']
+    
+    file_object = s3.get_object(Bucket=bucket_name, Key=file_name)
+    file_content = file_object["Body"].read()
+    
+    print(bucket_name, file_name)
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/pdf"
+        },
+        "body" : base64.b64encode(file_content),
+        "isBase64Encoded": True
+    }
